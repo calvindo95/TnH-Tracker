b00abe9c9d1153c89f43043ab5b41b76bda15aca
calvindo95 <calvindo95@gmail.com>
2023-09-26 17:21:55 -0500

    Updated client to have retry logic

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import config
 import requests
 import json
+import logging
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_sht31d.SHT31D(i2c)
@@ -22,11 +23,40 @@ def get_sensor_data():
         time.sleep(1)
         return get_sensor_data()
 
+def send_response():
+    send_response.count += 1
+    response = ''
+    
+    try:
+        response = requests.post(config.httpserverip, data=json.dumps(POST_DATA), headers=headers)
+        if response.text != "Received data value: 0":
+            logging.warning(f'failed - |{response.text}|')
+            return 1
+        else:
+            logging.info(f'Successfully posted data')
+            return 0
+        
+    except Exception as e:
+        logging.error(f'Error posting request: {e}')
+
+        if send_response.count > 60:
+            logging.critical(f"Failed to post request {send_response.count} times. Stopping")
+            return 1
+        else:
+            logging.error(f"Retrying sending post request count: {send_response.count} in 60 seconds")
+
+            time.sleep(60)
+            send_response()
+
 if __name__ == "__main__":
-    humidity, temperature = get_sensor_data()
+    logging.basicConfig(filename='/home/pi/TnH-Tracker/client.log', format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', encoding='utf-8', level=logging.INFO)
+    send_response.count = 0
+    
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
 
-    s = requests.Session()
+    humidity, temperature = get_sensor_data()
+    
+    # format post json
     POST_DATA = {'DeviceID':f'{config.deviceID}', 'hash':'pw_test', 'CurrentDateTime':f'{dt_string}', 'Temperature':f'{temperature}','Humidity':f'{humidity}'}
-    requests.post(config.httpserverip, data=json.dumps(POST_DATA), headers=headers)
+    send_response()
